b6c3076436402955b39df3a0ba24c98b25edab8f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-02 16:02:46 -0400

    day52

---
+++ src/day52_Abstraction/animalTask/Cow.java
@@ -0,0 +1,25 @@
+package day52_Abstraction.animalTask;
+
+public  class Cow extends Animal implements  Playable {
+
+
+    @Override
+    public void sleep() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void drink() {
+
+    }
+
+    @Override
+    public void play() {
+
+    }
+}

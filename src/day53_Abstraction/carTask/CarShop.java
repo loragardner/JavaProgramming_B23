b39e8b831db60aaf8a6144c41042f1200e7137b5
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-09 12:56:36 -0400

    day53

---
+++ src/day53_Abstraction/carTask/CarShop.java
@@ -0,0 +1,18 @@
+package day53_Abstraction.carTask;
+
+public class CarShop {
+
+    public static void main(String[] args) {
+
+        Honda honda =new Honda("Civic","dark blue",2018,19500);
+        Ford ford =new Ford("Mustang","black",2019,25800);
+        Porsche porsche=new Porsche("Cayman","red",2017,68000);
+        Audi audi =new Audi("A8","white",2020,83400);
+        Tesla tesla =new Tesla("S","blue",2021,120000);
+        Nio nio =new Nio("ET7","white",2020,68000);
+
+
+
+    }
+
+}
